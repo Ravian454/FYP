fae296c54ca1b00df669be24d399e606829fac8a
Ali Hamza <73699762+Ravian454@users.noreply.github.com>
2024-05-18 17:23:27 +0500

    Updated

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,13 +1,12 @@
-import Stripe from "stripe";
-import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import Stripe from 'stripe';
+import { NextRequest, NextResponse } from 'next/server';
 
-import { stripe } from "@/lib/stripe";
-import { db } from "@/lib/db";
+import { stripe } from '@/lib/stripe';
+import { db } from '@/lib/db';
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
+  const signature = req.headers.get('Stripe-Signature') as string;
 
   let event: Stripe.Event;
 
@@ -16,28 +15,36 @@ export async function POST(req: Request) {
       body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
-    )
+    );
   } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+    console.error('Webhook signature verification failed.', error.message);
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
   }
 
   const session = event.data.object as Stripe.Checkout.Session;
   const userId = session?.metadata?.userId;
   const courseId = session?.metadata?.courseId;
 
-  if (event.type === "checkout.session.completed") {
+  if (event.type === 'checkout.session.completed') {
     if (!userId || !courseId) {
+      console.error('Webhook Error: Missing metadata');
       return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
     }
 
-    await db.purchase.create({
-      data: {
-        courseId: courseId,
-        userId: userId,
-      }
-    });
+    try {
+      await db.purchase.create({
+        data: {
+          courseId: courseId,
+          userId: userId,
+        }
+      });
+    } catch (error) {
+      console.error('Database Error:', error);
+      return new NextResponse('Database Error', { status: 500 });
+    }
   } else {
-    return new NextResponse(`Webhook Error: Unhandled event type ${event.type}`, { status: 200 })
+    console.log(`Unhandled event type ${event.type}`);
+    return new NextResponse(`Webhook Error: Unhandled event type ${event.type}`, { status: 200 });
   }
 
   return new NextResponse(null, { status: 200 });
